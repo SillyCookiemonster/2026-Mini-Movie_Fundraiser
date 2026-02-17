d6617da2e53f9248ed2120d18b6a9ce2494946ca
SillyCookiemonster <meyere3109@masseyhigh.school.nz>
2026-02-18 09:44:41 +1300

    Basic statement generator

---
+++ C_01_Statement_Generator.py
@@ -0,0 +1,9 @@
+# functions go here
+def make_statement(statement, decoration):
+    """Emphasises headings by adding decoration
+    at the start and end"""
+
+    print(f"{decoration*3} {statement} {decoration*3}")
+
+# main routine goes here
+make_statement("Instructions", "ℹ️")
